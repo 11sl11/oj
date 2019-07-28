8f4fc3b9add96e52aced230d9407b9969ac54953
11sl11 <32123392+11sl11@users.noreply.github.com>
2019-07-28 11:19:18 +0800

    Create 求int型正整数在内存中存储时1的个数.java

---
+++ nowcoder/华为机试/求int型正整数在内存中存储时1的个数.java
@@ -0,0 +1,35 @@
+/**
+* 题目描述
+* 输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数。
+*
+* 输入描述：
+* 输入一个整数（int类型）
+*
+* 输出描述：
+* 这个数转换成2进制后，输出1的个数
+*
+* 示例
+*   输入
+*   5
+*
+*   输出
+*   2
+*
+*/
+
+import java.util.*;
+
+public class Main {
+    Scanner cin = new Scanner(System.in);
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    
+    void run() {
+        int x = cin.nextInt(), ans = 0;
+        while(x > 0) {
+            ans += (x&1);
+            x >>= 1;
+        } System.out.println(ans);
+    }
+}
