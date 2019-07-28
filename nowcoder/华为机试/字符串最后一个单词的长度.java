9332ced4c5ac3ed88815410afdc8ff8f79af9911
11sl11 <32123392+11sl11@users.noreply.github.com>
2019-07-28 09:10:35 +0800

    Create 字符串最后一个单词的长度.java

---
+++ nowcoder/华为机试/字符串最后一个单词的长度.java
@@ -0,0 +1,32 @@
+/**
+* 题目描述：
+* 计算字符串最后一个单词的长度，单词以空格隔开。
+*
+* 输入描述：
+* 一行字符串，非空，长度小于5000。
+*
+* 输出描述：
+* 整数N，最后一个单词的长度。
+*
+* 示例1
+*   输入
+*   hello world
+*   输出
+*   5
+*/
+
+import java.util.*;
+
+public class Main {
+    Scanner cin = new Scanner(System.in);
+    public static void main(String[] args) {
+        new Main().run();
+    }
+    
+    void run() {
+        String ss = null;
+        while(cin.hasNext()) {
+            ss = cin.next();
+        } System.out.println(ss.length());
+    }
+}
