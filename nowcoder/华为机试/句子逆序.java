67c2e3f70a6bab1da99280f9d1578b177d755e38
11sl11 <32123392+11sl11@users.noreply.github.com>
2019-08-02 10:57:30 +0800

    Update 句子逆序.java

--- nowcoder/华为机试/句子逆序.java
+++ nowcoder/华为机试/句子逆序.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         new Main().run();
     }
-    
+     
     void run() {
         List<String> arr = new ArrayList<>();
         while(cin.hasNext()) {
